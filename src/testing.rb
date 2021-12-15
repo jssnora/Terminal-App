984497e8444fe8e35ae2f571b8c43e9fbcc0303c
jssnora <js.s@live.com>
2021-12-15 15:41:39 +1100

    added method to validate city/postcode input

--- src/testing.rb
+++ src/testing.rb
@@ -17,8 +17,7 @@ def change_city
         input_city_name = prompt.ask("Please enter a city name:", required: true) do |q|
             q.modify :strip, :down
         end
-        input_city_name.gsub!(" ", "+")
-        $city_name = input_city_name
+        $city_name = input_city_name.gsub!(" ", "+")
         $postcode = nil
     when "Search by postcode"
         input_postcode = prompt.ask("Please enter a postcode:", convert: :integer) do |q|
@@ -30,23 +29,37 @@ def change_city
     end
 end
 
-# def validate_city($city_name, $postcode)
-#     case validate
-#     when $city_name
-#         response = HTTParty.get("http://api.openweathermap.org/data/2.5/weather?q=#{$city_name}&units=metric&appid=#{api_key}")
-#     when 
-        
-#     else
-        
-#     end
-# end
+def validate_city
+    api_key = ENV["api_key"]
+    if $postcode == nil
+        response = HTTParty.get("http://api.openweathermap.org/data/2.5/weather?q=#{$city_name},au&units=metric&appid=#{api_key}")
+        if response.parsed_response["cod"] == 404
+            puts "ERROR - City not found. Please enter a valid Australian city name"
+            change_city
+        else
+            true
+        end
+    elsif $city_name == nil 
+        response = HTTParty.get("http://api.openweathermap.org/data/2.5/weather?zip=#{$postcode},au&units=metric&appid=#{api_key}")
+        if response.parsed_response["cod"] == 404
+            puts "ERROR - Postcode not found. Please enter a valid Australian postcode"
+            change_city            
+        else
+            $city_name = response.parsed_response["name"].downcase
+            true
+        end
+    else
+        puts "Some error has occurred, try again"
+    end
+end
 
 change_city
+p validate_city
+p $city_name
 
-response = HTTParty.get("http://api.openweathermap.org/data/2.5/weather?q=#{$city_name}&units=metric&appid=#{api_key}")
+# response = HTTParty.get("http://api.openweathermap.org/data/2.5/weather?q=#{$city_name},au&units=metric&appid=#{api_key}")
 
-p response
-
+# p response
 # exit_chosen = false
 # while !exit_chosen
 #     choices = ["Change city", "Today's weather", "7 Day forecast" , "Exit"]
